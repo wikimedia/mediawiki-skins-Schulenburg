14f35c9e641a70447abcd8282ebe66269236eda0
Lewis Cawte <lewis@lewiscawte.me>
2014-08-22 19:26:18 +0100

    Add extension credits.
    Change-Id: Id6aeebad3b1987761d9ec6d05ac6a7cb7c43a8d8

--- Schulenburg.php
+++ Schulenburg.php
@@ -1,7 +1,26 @@
 <?php
+/**
+ * Schulenburg skin.
+ *
+ * @file
+ * @ingroup Skins
+ * @author Tim Starling
+ * @license http://www.gnu.org/copyleft/gpl.html GNU General Public License 2.0 or later
+ *
+ * To install place the Schulenburg folder (the folder containing this file!) into
+ * skins/ and add this line to your wiki's LocalSettings.php:
+ * require_once("$IP/skins/Schulenburg/Schulenburg.php");
+ */
+
+$wgExtensionCredits['skin'][] = array(
+	'path' => __FILE__,
+	'name' => 'Schulenburg',
+	'author' => 'Tim Starling',
+	'description' => 'Schulenburg',
+	'url' => 'https://www.mediawiki.org/wiki/Skin:Schulenburg',
+);
 
 $wgValidSkinNames['schulenburg'] = 'Schulenburg';
 $wgSkipSkins[] = 'schulenburg';
 $wgSchulenburgSkinPath = false;
 $wgAutoloadClasses['SkinSchulenburg'] = __DIR__ . '/Schulenburg.class.php';
-
