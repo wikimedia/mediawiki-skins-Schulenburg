efdbfbf7479649370b01a86326634baa79634ad8
Umherirrender <umherirrender_de.wp@web.de>
2021-10-24 16:17:41 +0200

    Emit deprecation warning from php entry point
    Change-Id: I0d220a5ee8b6adf93a6392ad3d8765e9c10a7522

--- Schulenburg.php
+++ Schulenburg.php
@@ -14,10 +14,10 @@
 
 if ( function_exists( 'wfLoadSkin' ) ) {
 	wfLoadSkin( 'Schulenburg' );
-	/*wfWarn(
+	wfWarn(
 		'Deprecated PHP entry point used for Schulenburg skin. Please use wfLoadSkin instead, ' .
 		'see https://www.mediawiki.org/wiki/Extension_registration for more details.'
-	);*/
+	);
 	return;
 } else {
 	die( 'This version of the Schulenburg skin requires MediaWiki 1.25+' );
